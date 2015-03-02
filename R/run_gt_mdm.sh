5b06a12572a9993ad5dccfdf9362ccd7245de281
Steven Wu <stevenhwu@gmail.com>
2015-03-02 14:20:57 -0700

    Update dirty check

--- R/run_gt_mdm.sh
+++ R/run_gt_mdm.sh
@@ -2,9 +2,7 @@
 
 #base_counts_hets_CEU10_878_byref_flag_filtered.txt       base_counts_homos_ref_CEU10_878_byref_flag_filtered.txt
 #base_counts_homos_alt_CEU10_878_byref_flag_filtered.txt
-#PWD=`pwd`
-#MdmFile="/home/steven/Postdoc2/Project_MDM/RCode/mdm.R"
-#GtMdm="/home/steven/Postdoc2/Project_MDM/RCode/search-gt-mdm.R"
+
 
 if [ -h $0 ]
 then
@@ -14,13 +12,11 @@ else
 	Orig=$0
 	PWD=`pwd`
 fi
-
-if [ $# -eq 1 ] 
+echo $1 $#
+if [[ $# -eq 1  &&  $1 == "D" || $1 == "d" ]]
 then
-	if [ "$1" == "D" | "$1" == "d" ]
-	then
-		isDirty="d"
-	fi
+	echo $1
+	isDirty="d"
 fi
 
 MdmFile="${PWD}/mdm.R"
@@ -45,7 +41,7 @@ for f in ${Files}
 do
 	for numCom in $(seq 1 6)
 	do
-		echo $f $numCom
+		echo "$f ${numTrial} ${numCom}${isDirty}"
 		Rscript search-gt-mdm.R ${numTrial} ${numCom}${isDirty} $f
 	done
 	
