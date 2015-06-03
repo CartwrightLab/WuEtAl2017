bc202cd6edd8908bab33728c74145602bb90f6a8
Steven Wu <stevenhwu@gmail.com>
2015-06-03 15:12:28 -0700

    Add old coverage analyses

---
+++ depth/createCoverage.sh
@@ -0,0 +1,7 @@
+#samtools view -b /storage/CEUTrio/20130906/NA12878.mapped.ILLUMINA.bwa.CEU.high_coverage_pcr_free.20130906.bam 21 > CEU13_878_C21.bam
+#~/bin/bedtools genomecov -bga -ibam CEU13_878_C21.bam > coverage_13_878_21
+
+#samtools view -b /storage/CEUTrio/20130906/NA12891.mapped.ILLUMINA.bwa.CEU.high_coverage_pcr_free.20130906.bam 21 > CEU13_878_C21.bam
+#~/bin/bedtools genomecov -bga -ibam CEU13_878_C21.bam > coverage_13_878_21
+
+
